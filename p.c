18976f8a4c5cac48e5d80fb2a68bf55f5363721f
Piyushayush21 <piyushayush638@gmail.com>
2026-06-02 15:53:41 +0530

    Initial commit

---
+++ p.c
@@ -0,0 +1,35 @@
+// #include <stdio.h>
+// // this is c laguage code implmentation structure
+// int main() {
+//     printf("Hello");
+//     int number = 25;
+//     char star = '*';
+//     int age = 24;
+//     float pi = 3.14;
+//     return 0;
+//     printf("age ");
+//     return 0;
+
+
+
+// }
+
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello\n");
+
+    int number = 25;
+    char star = '*';
+    int age = 24;
+    float pi = 3.14;
+
+    printf("Number = %d\n", number);
+    printf("Star = %c\n", star);
+    printf("Age = %d\n", age);
+    printf("Pi = %.2f\n", pi);
+
+    return 0;
+}

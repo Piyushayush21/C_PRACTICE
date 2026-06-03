c45f574d15b36fba2efb01f4b03b0cf578613824
Piyushayush21 <piyushayush638@gmail.com>
2026-06-03 19:44:07 +0530

    c_language

--- second.c
+++ second.c
@@ -12,11 +12,27 @@
 
 // #include <stdio.h>
 
-int main() {
+// int main() {
 
-    int num = (12,3,4,5,6,7,123);
-    printf("Number = %d\n", num);
-    char name[] = "PIYUSH AYUSH";
-    printf("Name = %s\n", name);
+//     int num = (12,3,4,5,6,7,123);
+//     printf("Number = %d\n", num);
+//     char name[] = "PIYUSH AYUSH";
+//     printf("Name = %s\n", name);
+//     return 0;
+// }
+
+
+//  taking input from user 
+
+    #include <stdio.h>
+
+int main() {
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("this is your name %s\n", name);
+
     return 0;
 }

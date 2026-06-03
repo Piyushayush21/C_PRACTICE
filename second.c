085f6d4c91a40f3af5f9a431f1c9175e2fa79c04
Piyushayush21 <piyushayush638@gmail.com>
2026-06-03 19:22:33 +0530

    c_language

--- second.c
+++ second.c
@@ -1,12 +1,22 @@
-# include<stdio.h> 
 
-//area of square
+
+// # include<stdio.h>
+// int main() {
+
+//     int num = (12,3,4,5,6,7,123);
+//     printf("d%" , num);
+//     char name = ("PIYUSH AYUSH");
+//     printf("c%", name );
+//     return 0;
+// }
+
+// #include <stdio.h>
+
 int main() {
 
-    int side;
-    scanf("%d", &side);
-    printf("%d", side * side);
+    int num = (12,3,4,5,6,7,123);
+    printf("Number = %d\n", num);
+    char name[] = "PIYUSH AYUSH";
+    printf("Name = %s\n", name);
     return 0;
 }
-
-

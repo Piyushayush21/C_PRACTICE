e4fb213207d4c0ec7a2754b15d6ee0df9824746a
Piyushayush21 <piyushayush638@gmail.com>
2026-06-05 18:59:20 +0530

    c_language

--- INSTRUCTION_OPERATORS.c
+++ INSTRUCTION_OPERATORS.c
@@ -1,8 +1,20 @@
 //DECALARATION INSTRUCTION>>> : decalre variable before using it 
+// # include<stdio.h>
+// int main() {
+//     int a = 980;
+//     int b = a;
+//     printf("%d" , b);
+//     return 0;
+// }
+
+// write a programe if a number is divisible by 2 or not 
+
+
 # include<stdio.h>
 int main() {
-    int a = 980;
-    int b = a;
-    printf("%d" , b);
+    int X;
+    printf("ENTER A NUMBER");
+    scanf("%d", & X);
+    printf("%d" , X % 2 == 0);
     return 0;
 }

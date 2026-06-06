a9400f12881cfefb192f1fc4a14fc35ae4bacbff
Piyushayush21 <piyushayush638@gmail.com>
2026-06-06 14:02:11 +0530

    CONDITIONAL_STATEMENTS

--- CS.c
+++ CS.c
@@ -1,14 +1,29 @@
-// CONDITIOANAL OPPERATORS 
-# include<stdio.h>
-int main () {
-  int age;
-  printf("enter age :");
-  scanf("%d" , &age);
-if (age != 19) {
-    printf("who are you");
-}
-else {
-    printf("how are you ");
-}
+// // CONDITIOANAL OPPERATORS 
+// # include<stdio.h>
+// int main () {
+//   int age;
+//   printf("enter age :");
+//   scanf("%d" , &age);
+// if (age != 19) {
+//     printf("who are you");
+// }
+// else {
+//     printf("how are you ");
+// }
   
+// }
+
+
+# include <stdio.h>
+int main() {
+    int age;
+    printf("enter age:");
+    scanf("%d" , & age);
+    if(age > 19) {
+        printf("YOU ARE UNEMPLOYED \n");
+        printf("WHO'S GONNA MARRY YOU \n");
+    }
+    else {
+        printf("YOU ARE NOT WORTHY OF THOSE QUESTONS BOY");
+    }
 }

b6f456a82801ac8861aa0fd19dbf5d0f44047d80
Piyushayush21 <piyushayush638@gmail.com>
2026-06-06 13:52:42 +0530

    CONDITONAL_STATEMENTS

---
+++ CS.c
@@ -0,0 +1,14 @@
+// CONDITIOANAL OPPERATORS 
+# include<stdio.h>
+int main () {
+  int age;
+  printf("enter age :");
+  scanf("%d" , &age);
+if (age != 19) {
+    printf("who are you");
+}
+else {
+    printf("how are you ");
+}
+  
+}
--- INSTRUCTION_OPERATORS.c
+++ INSTRUCTION_OPERATORS.c
@@ -10,15 +10,15 @@
 // write a programe if a number is divisible by 2 or not 
 
 
-# include<stdio.h>
-int main() {
-    int X;
-    printf("ENTER A NUMBER");
-    scanf("%d", &X);
-    printf("%s" , (X % 2 == 0 ) ? "EVEN" : "ODD"  );
+// # include<stdio.h>
+// int main() {
+//     int X;
+//     printf("ENTER A NUMBER");
+//     scanf("%d", &X);
+//     printf("%s" , (X % 2 == 0 ) ? "EVEN" : "ODD"  );
      
-    return 0;
-}
+//     return 0;
+// }
 
 
 
@@ -31,6 +31,44 @@ int main() {
 //     scanf("%d", &X);
 
 //     printf("%s", (X % 2 == 0) ? "EVEN" : "ODD");
+
+//     return 0;
+// }
+
+// write a programe to print the avg of 3 numbers 
+
+# include<stdio.h>
+int main() {
+    int X,Y,Z;
+    printf("ENTER A NUMBER: ");
+    scanf("%d", &X);
+
+    printf("ENTER A NUMBER: ");
+    scanf("%d", &Y);
+
+    printf("ENTER A NUMBER: ");
+    scanf("%d", & Z);
+
+    printf("Average = %d" , (X + Y + Z )/3 );
+    return 0;
+}
+
+
+#include <stdio.h>
+
+// int main() {
+//     int X, Y, Z;
+
+//     printf("ENTER FIRST NUMBER: ");
+//     scanf("%d", &X);
+
+//     printf("ENTER SECOND NUMBER: ");
+//     scanf("%d", &Y);
+
+//     printf("ENTER THIRD NUMBER: ");
+//     scanf("%d", &Z);
+
+//     printf("Average = %d", (X + Y + Z) / 3);
 
 //     return 0;
 // }

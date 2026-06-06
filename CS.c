d53e8c1291a1bd737f4c6fd4403b279319bec49e
Piyushayush21 <piyushayush638@gmail.com>
2026-06-06 14:31:16 +0530

    CONDITIONAL_STATEMENTS

--- CS.c
+++ CS.c
@@ -14,16 +14,46 @@
 // }
 
 
+// # include <stdio.h>
+// int main() {
+//     int age;
+//     printf("enter age:");
+//     scanf("%d" , & age);
+//     if(age > 19) {
+//         printf("YOU ARE UNEMPLOYED \n");
+//         printf("WHO'S GONNA MARRY YOU \n");
+//     }
+//     else {
+//         printf("YOU ARE NOT WORTHY OF THOSE QUESTONS BOY");
+//     }
+// }
+
+
+// # include <stdio.h>
+// int main() {
+//     int age;
+//     printf("ENTER AGE:");
+//     scanf("%d" , &age);
+//     if (age >= 18) {
+//         printf("adult\n");
+//     }
+//     else if (age > 13 && age < 18){
+//         printf("teenager \n");
+//     }
+//     else {
+//         printf("child");
+//     }
+//     return 0;
+
+// }
+
+//  [TERNARY OPERATOR]
+
 # include <stdio.h>
 int main() {
     int age;
-    printf("enter age:");
-    scanf("%d" , & age);
-    if(age > 19) {
-        printf("YOU ARE UNEMPLOYED \n");
-        printf("WHO'S GONNA MARRY YOU \n");
-    }
-    else {
-        printf("YOU ARE NOT WORTHY OF THOSE QUESTONS BOY");
-    }
+    printf("ENTER AGE:");
+    scanf("%d" , &age);
+    age >18 ? printf("adult \n") : printf("teenn,child \n");
+    
 }
